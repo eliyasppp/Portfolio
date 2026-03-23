d23de5a24ea721a5b28e68a1b5add5f6b4f62de1
Eliyas <peliyas661@gmail.com>
2026-03-23 16:21:18 +0530

    initial

---
+++ JS-Task_files/index.js
@@ -0,0 +1,12 @@
+var num=prompt("Enter Your Mark");
+
+if(90<=num){
+    console.log("Grade A");  
+}
+elseif(75<=num);{
+    console.log("Grade B");  
+}
+elseif(85<=num);{
+    console.log("Grade B");  
+}
+
